a60e39ead4f4f55c6da8e7e623d71e0c2151611c
Victorcvaz <102685506+Victorcvaz@users.noreply.github.com>
2022-07-07 18:46:53 -0300

    Function P2

--- FunctionParty2.py
+++ FunctionParty2.py
@@ -74,3 +74,60 @@ print(f"A outside first output {a}")
 tests(a)
 print(f"A outside second output {a}")
 """
+
+# Python return
+
+"""
+def Sum(a=0,b=0,c=0):
+    s = a+b+c
+    return s
+    s = 0 # stop the function after call return
+print(Sum(3,2,5), Sum(2,2), Sum(6))
+r1 = Sum(15,10)
+print(r1)
+"""
+
+# Python exercise basic 01
+
+"""
+def fatorial(n=1):
+    f = 1
+    for c in range(n, 0, -1):
+        f*=c
+    return f
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f"the results are {f1}, {f2} and {f3}")
+
+# Python exercise basic 02
+
+def test(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+if test(int(input("Number: "))):
+    print("Is pair")
+else:
+    print("Is Odd")
+"""
+
+# Challenge 101
+
+"""
+from datetime import datetime
+
+def vote(year):
+    years = datetime.now().year - int(year)
+    if years < 16:
+        return f'\033[1;33mYour age is {years}, Vote Denied\033[m'
+    elif years > 18 and years < 60:
+        return f'\033[1;33mYour age is {years}, Mandatory vote\033[m'
+    else:
+        return f'\033[1;33mYour age is {years}, Optional Vote\033[m'
+result = vote(input("Birth year: "))
+print('\033[1;36m~\033[m' * (len(result)+4))
+print(f"{result:^{len(result)+4}}")
+print('\033[1;36m~\033[m' * (len(result)+4))
+"""
